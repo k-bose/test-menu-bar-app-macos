e90cd01810829f0660d71622703d30e0e39ac961
k-bose <k-bose@outlook.com>
2023-06-09 12:32:19 +0530

    Initial Commit

---
+++ test-menu-bar-app-macos/test_menu_bar_app_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_menu_bar_app_macosApp.swift
+//  test-menu-bar-app-macos
+//
+//  Created by Son Goku on 09-06-2023.
+//
+
+import SwiftUI
+
+@main
+struct test_menu_bar_app_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
