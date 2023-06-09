e316f87b0f55ec33e99d7a243c3006f0ebb0b636
k-bose <k-bose@outlook.com>
2023-06-09 15:18:36 +0530

    Adding `NSStatusItem`

--- test-menu-bar-app-macos/test_menu_bar_app_macosApp.swift
+++ test-menu-bar-app-macos/test_menu_bar_app_macosApp.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 @main
-struct test_menu_bar_app_macosApp: App {    
+struct test_menu_bar_app_macosApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         Settings {
             EmptyView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusBarItem: NSStatusItem!
+    var menu: NSMenu!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusBarItem.button?.image = NSImage(named: "YourIconName") // replace "YourIconName" with your actual icon name
+
+        menu = NSMenu(title: "My Menu Bar App")
+        let menuItem = NSMenuItem(title: "Open", action: #selector(AppDelegate.openApp), keyEquivalent: "")
+        menuItem.target = self
+        menu.addItem(menuItem)
+
+        statusBarItem.menu = menu
+    }
+
+    @objc func openApp() {
+        // Add the code to open your application window here.
     }
 }
